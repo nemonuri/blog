779f3a7689ce19386e3086b2fb870077357651c4
nemonuri <nemonuri2@gmail.com>
2025-06-09 23:56:50 +0900

    GetSiteDirectory 구현

--- src/Nemonuri.BlogTools/Program.cs
+++ src/Nemonuri.BlogTools/Program.cs
@@ -1,13 +1,36 @@
-﻿using Markdig;
+﻿using System.Reflection;
+using Markdig;
 
 MarkdownPipeline pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
 
-string docDirectoryPath = Path.Combine(AppContext.BaseDirectory, "doc");
+DirectoryInfo docDirectory = new DirectoryInfo(Path.Combine(AppContext.BaseDirectory, "doc"));
+DirectoryInfo siteDirectory = GetSiteDirectory();
+if (siteDirectory.Exists) { siteDirectory.Delete(recursive: true); }
 
-foreach (string filePath in Directory.EnumerateFiles(docDirectoryPath, "*.md", SearchOption.AllDirectories))
+siteDirectory.Create();
+
+//--- Create index.html ---
+//---|
+
+foreach (FileInfo file in docDirectory.EnumerateFiles("*.md", SearchOption.AllDirectories))
 {
-    string markDownText = File.ReadAllText(filePath);
+    string markDownText = File.ReadAllText(file.FullName);
+
+    //Path.re
+
     var result = Markdown.ToHtml(markDownText, pipeline);
-    Console.WriteLine(filePath);
+
+    //Markdown.Parse()
+    //Console.WriteLine(filePath);
     Console.WriteLine(result);
+}
+
+
+static DirectoryInfo GetSiteDirectory()
+{
+    string siteDirectory =
+        typeof(Program).Assembly.GetCustomAttributes<AssemblyMetadataAttribute>().FirstOrDefault(a => a.Key == "SiteDirectory")?.Value
+        ?? throw new InvalidOperationException("""Cannot fine "SiteDirectory" AssemblyMetadata.""");
+
+    return new DirectoryInfo(siteDirectory);
 }
