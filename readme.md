eea9545ff425219dc881c78ae40cedbe7ed98e7c
nemonuri <nemonuri2@gmail.com>
2025-06-13 15:42:27 +0900

    - readme.md 추가 - 내 블로그 연결 - 불필요한 코드 제거 - 함의 추가

---
+++ readme.md
@@ -0,0 +1,3 @@
+# 네모누리의 블로그
+
+https://nemonuri.github.io/blog/
--- src/Nemonuri.BlogTools/YamlFrontMatterRawData.cs
+++ src/Nemonuri.BlogTools/YamlFrontMatterRawData.cs
@@ -2,21 +2,6 @@ using YamlDotNet.Serialization;
 
 namespace Nemonuri.BlogTools;
 
-#if false
-[YamlSerializable]
-public struct YamlFrontMatterRawData
-{
-    [YamlMember(Alias = "title")]
-    public string? Title;
-
-    [YamlMember(Alias = "date")]
-    public DateTime Date;
-
-    [YamlMember(Alias = "daily_index")]
-    public int? DailyIndex;
-}
-#endif
-
 [YamlSerializable]
 public struct DiaryYamlFrontMatterRawData
 {
