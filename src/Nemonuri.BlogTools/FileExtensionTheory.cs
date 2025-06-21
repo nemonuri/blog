3d93748a405e4b62df9affb052288c682f0bc733
nemonuri <nemonuri2@gmail.com>
2025-06-21 18:54:07 +0900

    ChangeExtension 메서드에 XML 주석 추가

--- src/Nemonuri.BlogTools/FileExtensionTheory.cs
+++ src/Nemonuri.BlogTools/FileExtensionTheory.cs
@@ -2,6 +2,12 @@ namespace Nemonuri.BlogTools;
 
 public static class FileExtensionTheory
 {
+    /// <summary>
+    /// 원본 파일 경로의 확장자를 새 것으로 교체합니다.
+    /// </summary>
+    /// <param name="originalPath">원본 파일 경로</param>
+    /// <param name="extension">교체할 새 확장자</param>
+    /// <returns>확장자가 교체된 파일 경로</returns>
     public static string ChangeExtension(string originalPath, string? extension)
     {
         int lastDotIndex = originalPath.LastIndexOf('.');
